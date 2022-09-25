bc4766350663af9fa3b8d495577d8fd5356ddb74
Soufiane Nouae <snouae@e2r2p10.1337.ma>
2022-09-25 15:25:33 +0100

    draw

--- main.c
+++ main.c
@@ -6,30 +6,30 @@
 /*   By: snouae <snouae@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 09:02:29 by snouae            #+#    #+#             */
-/*   Updated: 2022/09/25 14:46:47 by snouae           ###   ########.fr       */
+/*   Updated: 2022/09/25 15:23:14 by snouae           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 
-// void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-// {
-//     char    *dst;
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+    char    *dst;
 
-//     dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-//     *(unsigned int*)dst = color;
-// }
-// int zoom = 30;
-// void draw_map(t_map *map)
-// {
-// 	map->mlx_ptr = mlx_init();
-// 	map->win_ptr = mlx_new_window(map->mlx_ptr, map->rows * 20, map->big_width * 20, "cub3d");
-// 	map->data.img = mlx_new_image(map->win_ptr, map->rows * 20, map->big_width * 20);
-// 	map->data.addr = mlx_get_data_addr(map->data.img, &map->data.bits_per_pixel, &map->data.line_length, &map->data.endian);
-// 	mlx_put_image_to_window(map->mlx_ptr, map->win_ptr, map->data.img, 0, 0);
-// 	mlx_loop(map->mlx_ptr);
-// }
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
+}
+int zoom = 30;
+void draw_map(t_map *map)
+{
+	map->mlx_ptr = mlx_init();
+	map->win_ptr = mlx_new_window(map->mlx_ptr, map->big_width * 10, map->rows * 10, "cub3d");
+	//map->data.img = mlx_new_image(map->win_ptr, map->rows * 20, map->big_width * 20);
+	//map->data.addr = mlx_get_data_addr(map->data.img, &map->data.bits_per_pixel, &map->data.line_length, &map->data.endian);
+	//mlx_put_image_to_window(map->mlx_ptr, map->win_ptr, map->data.img, 0, 0);
+	mlx_loop(map->mlx_ptr);
+}
 
 int main(int ac, char **av)
 {
@@ -55,5 +55,5 @@ int main(int ac, char **av)
 		printf("The map is incorrect, reconfigure it !!!!!!!!!!!");
 		return (1);
 	}
-	//draw_map(&map);
+	draw_map(&map);
 }
